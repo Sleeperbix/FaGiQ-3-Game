ce0a25869f214abab8a9cadce75522553dfe37e9
Sleeperbix <peter.lindley@outlook.com>
2025-08-14 16:40:23 +0100

    Added CategoryImporter, other edits to json files

--- Scripts/BackgroundWander.cs
+++ Scripts/BackgroundWander.cs
@@ -3,43 +3,43 @@ using System;
 
 public partial class BackgroundWander : Node
 {
-    [Export] public TextureRect backgroundImage;
-    [Export] public float moveDuration = 5.0f;
+	[Export] public TextureRect backgroundImage;
+	[Export] public float moveDuration = 5.0f;
 
-    private Random random = new();
+	private Random random = new();
 
-    public override void _Ready()
-    {
-        if (backgroundImage == null)
-        {
-            backgroundImage = GetNode<TextureRect>("BGImage");
-        }
+	public override void _Ready()
+	{
+		if (backgroundImage == null)
+		{
+			backgroundImage = GetNode<TextureRect>("BGImage");
+		}
 
-        Wander();
-    }
+		Wander();
+	}
 
-    private void Wander()
-    {
-        Vector2 screenSize = GetViewport().GetVisibleRect().Size;
-        Vector2 imageSize = backgroundImage.Size;
+	private void Wander()
+	{
+		Vector2 screenSize = GetViewport().GetVisibleRect().Size;
+		Vector2 imageSize = backgroundImage.Size;
 
-        Vector2 maxOffset = (imageSize - screenSize) / 2f;
+		Vector2 maxOffset = (imageSize - screenSize) / 2f;
 
-        maxOffset.X = Mathf.Max(0, maxOffset.X);
-        maxOffset.Y = Mathf.Max(0, maxOffset.Y);
+		maxOffset.X = Mathf.Max(0, maxOffset.X);
+		maxOffset.Y = Mathf.Max(0, maxOffset.Y);
 
-        Vector2 targetOffset = new Vector2((float)(random.NextDouble() * 2 - 1) * maxOffset.X,
-                                           (float)(random.NextDouble() * 2 - 1) * maxOffset.Y
-        );
+		Vector2 targetOffset = new Vector2((float)(random.NextDouble() * 2 - 1) * maxOffset.X,
+										   (float)(random.NextDouble() * 2 - 1) * maxOffset.Y
+		);
 
-        Vector2 centredPosition = (screenSize - imageSize) / 2f;
-        Vector2 targetPosition = centredPosition + targetOffset;
+		Vector2 centredPosition = (screenSize - imageSize) / 2f;
+		Vector2 targetPosition = centredPosition + targetOffset;
 
-        Tween tween = CreateTween();
-        tween.TweenProperty(backgroundImage, "position", targetPosition, moveDuration)
-            .SetTrans(Tween.TransitionType.Sine)
-            .SetEase(Tween.EaseType.InOut);
+		Tween tween = CreateTween();
+		tween.TweenProperty(backgroundImage, "position", targetPosition, moveDuration)
+			.SetTrans(Tween.TransitionType.Sine)
+			.SetEase(Tween.EaseType.InOut);
 
-        tween.TweenCallback(Callable.From(() => { Wander(); }));
-    }
+		tween.TweenCallback(Callable.From(() => { Wander(); }));
+	}
 }
--- Scripts/Managers/ManagerAudio.cs
+++ Scripts/Managers/ManagerAudio.cs
@@ -3,41 +3,41 @@ using System;
 
 public partial class ManagerAudio : Node
 {
-    public static ManagerAudio Instance;
-    private AudioStreamPlayer musicPlayer;
-    private AudioStreamPlayer sfxPlayer;
+	public static ManagerAudio Instance;
+	private AudioStreamPlayer musicPlayer;
+	private AudioStreamPlayer sfxPlayer;
 
-    public override void _Ready()
-    {
-        Instance = this;
-        musicPlayer = GetNode<AudioStreamPlayer>("MusicPlayer");
-        sfxPlayer = GetNode<AudioStreamPlayer>("SFXPlayer");
-    }
+	public override void _Ready()
+	{
+		Instance = this;
+		musicPlayer = GetNode<AudioStreamPlayer>("MusicPlayer");
+		sfxPlayer = GetNode<AudioStreamPlayer>("SFXPlayer");
+	}
 
-    public void PlayMusic(string import)
-    {
-        AudioStream music = GD.Load<AudioStream>(import);
-        musicPlayer.Stream = music;
-        musicPlayer.Play();
-    }
+	public void PlayMusic(string import)
+	{
+		AudioStream music = GD.Load<AudioStream>(import);
+		musicPlayer.Stream = music;
+		musicPlayer.Play();
+	}
 
-    public void PlayMusicLoop(string import)
-    {
-        AudioStream music = GD.Load<AudioStream>(import);
-        if (music is AudioStreamMP3 mp3) { mp3.Loop = true; }
-        musicPlayer.Stream = music;
-        musicPlayer.Play();
-    }
+	public void PlayMusicLoop(string import)
+	{
+		AudioStream music = GD.Load<AudioStream>(import);
+		if (music is AudioStreamMP3 mp3) { mp3.Loop = true; }
+		musicPlayer.Stream = music;
+		musicPlayer.Play();
+	}
 
-    public void PlaySFX(string import)
-    {
-        AudioStream sound = GD.Load<AudioStream>(import);
-        sfxPlayer.Stream = sound;
-        sfxPlayer.Play();
-    }
+	public void PlaySFX(string import)
+	{
+		AudioStream sound = GD.Load<AudioStream>(import);
+		sfxPlayer.Stream = sound;
+		sfxPlayer.Play();
+	}
 
-    public void StopMusic()
-    {
-        musicPlayer.Stop();
-    }
+	public void StopMusic()
+	{
+		musicPlayer.Stop();
+	}
 }
